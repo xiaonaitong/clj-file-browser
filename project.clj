a65b569cca8319149e536fe6934b6a1bcd6526f5
xiaonaitong <xiaonaitong@gmail.com>
2012-08-28 20:21:27 +0800

    turn scratch files into runnable project

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [seesaw "1.4.2"]
+                 [com.jgoodies/looks "2.2.2"]]
+  :java-source-path "jvm"
+  :resources-path "resources"
+  :main clj-file-browser.core)
--- src/clj_file_browser/repl.clj
+++ src/clj_file_browser/repl.clj
@@ -1,6 +1,5 @@
-(ns clj-file-browser.core
-  (:gen-class)
-  (:use (seesaw core tree))
+(ns clj-file-browser.repl
+  (:use (seesaw core tree ))
   (:require [seesaw.bind :as b])
   (:use clojure.java.io)
   (:use seesaw.mig)
@@ -17,6 +16,11 @@
                                                         :hscroll :never)
                                                   :one-touch-expandable? true))
 
+(comment (config! browser :content (left-right-split (tree :id :dirs)
+                                             (listbox :id :files
+                                                      :model (range 10))
+                                             :one-touch-expandable? true)))
+
 (def root (java.io.File. "."))
 
 (config! (select browser [:#dirs]) :model (simple-tree-model #(.isDirectory %) #(->> % .listFiles (filter (memfn isDirectory) )) (java.io.File. ".")))
@@ -27,11 +31,49 @@
    (getTreeCellRendererComponent [t value s e l r h]
      (do
        (proxy-super getTreeCellRendererComponent t value s e l r h)
-       (proxy-super setOpenIcon (icon (resource "icons/open.png")))
-       (proxy-super setClosedIcon (icon (resource "icons/closed.png")))
+       (proxy-super setOpenIcon (icon (file "icons/open.png")))
+       (proxy-super setClosedIcon (icon (file "icons/closed.png")))
        (proxy-super setText (.getName value)))
      this)))
 
+(require '[seesaw.bind :as b])
+
+(comment (b/bind
+  (b/selection (select browser [:#dirs]))
+  (b/transform (fn [files]
+                 (when (seq files)
+                   (let [current (last files)]
+                     (when (.isDirectory current)
+                       (vec (map #(.getName %) (.listFiles current))))))))
+  (b/property (select browser [:#files]) :model)))
+
+
+(comment (b/bind
+  (b/selection (select browser [:#dirs]))
+  (b/transform (fn [files]
+                 (when (seq files)
+                   (let [current (last files)]
+                     (when (.isDirectory current)
+                       (vec (.listFiles current)))))))
+  (b/b-do [files]
+          (let [files-pane (select browser [:#files])]
+            (.removeAll files-pane)
+            (doseq [f files]
+              (let [file-icon (-> (if (.isDirectory f)
+                                    "icons/folder.png"
+                                    "icons/document.png")
+                                  file
+                                  icon)
+                    file-name (apply str (take 20 (.getName f)))]
+                (.add files-pane (label :text file-name
+                                        :icon  file-icon
+                                        :v-text-position :bottom
+                                        :h-text-position  :center))))
+            (doto files-pane
+              (.revalidate )
+              (.repaint))))))
+
+;;; refactor out refresh files pane
 (defn refresh-files-view [files]
   (let [files-pane (select browser [:#files])]
     (.removeAll files-pane)
@@ -39,7 +81,7 @@
       (let [file-icon (-> (if (.isDirectory f)
                             "icons/folder.png"
                             "icons/document.png")
-                          resource icon)
+                          file icon)
             file-name (apply str (take 20 (.getName f)))]
         (.add
          files-pane
@@ -52,7 +94,9 @@
                            (when (and (= (.getClickCount e) 2)
                                     (not (.isConsumed e))
                                     (.isDirectory f))
-                             (refresh-files-view (.listFiles f))))]))))
+                             (refresh-files-view (.listFiles f))
+                             (b/selection! (select browser [:#dirs])
+                                           (path-to f root))))]))))
     (doto files-pane
       (.revalidate )
       (.repaint))))
@@ -77,15 +121,11 @@
                               (vec (.listFiles current)))))))
          (b/b-do* refresh-files-view)))
 
-(comment (def b2 (b/bind
-          (b/selection (select browser [:#dirs]))
-          (b/transform (fn [files]
-                         (when (seq files)
-                           (let [current (last files)]
-                             (when (.isDirectory current)
-                               (vec (map #(.getName %) (.listFiles current))))))))
-          (b/property (select browser [:#files]) :model))))
-
-(defn -main [& args]
-  (-> browser
-      show!))
+(def b2 (b/bind
+         (b/selection (select browser [:#dirs]))
+         (b/transform (fn [files]
+                        (when (seq files)
+                          (let [current (last files)]
+                            (when (.isDirectory current)
+                              (vec (map #(.getName %) (.listFiles current))))))))
+         (b/property (select browser [:#files]) :model)))
